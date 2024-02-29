81cd395306c5c3f977151b8fe13dedd1fb0e84a8
filipposkanellos <filippos.kanellos@gmail.com>
2024-02-29 12:58:16 +0200

    clean up

--- app/src/main/java/gr/sportsbook/presentation/ui/components/AppIcon.kt
+++ app/src/main/java/gr/sportsbook/presentation/ui/components/AppIcon.kt
@@ -1,21 +1,19 @@
 package gr.sportsbook.presentation.ui.components
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppIcon(
+    modifier: Modifier,
     @DrawableRes iconRes: Int,
     contentDescription: String?,
-    tint: Color = MaterialTheme.colorScheme.onSurface,
-    modifier: Modifier = Modifier.size(24.dp)
+    tint: Color = MaterialTheme.colorScheme.onSurface
 ) {
     Icon(
         painter = painterResource(id = iconRes),
--- app/src/main/java/gr/sportsbook/presentation/ui/composites/SwitchWithIcon.kt
+++ app/src/main/java/gr/sportsbook/presentation/ui/composites/SwitchWithIcon.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -50,6 +51,7 @@ fun SwitchWithIcon(viewModel: MainViewModel) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             AppIcon(
+                modifier = Modifier.size(24.dp),
                 iconRes = if (isDarkTheme) ThemeIcon.DARK_MODE.drawableRes else ThemeIcon.LIGHT_MODE.drawableRes,
                 contentDescription = "Toggle Dark Mode",
                 tint = contentColor
